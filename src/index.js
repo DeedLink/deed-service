6d1ed9e92bfe958436a054c126f3f87ef2602331
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-07 14:44:15 +0530

    allow everything

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/deeds", deedRoutes);
