858ab6f10f1e976427da4971ebd22262577d544e
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-09 23:52:51 +0530

    plan insert update

--- src/controllers/deedController.js
+++ src/controllers/deedController.js
@@ -543,7 +543,6 @@ export const insertPlan = async (req, res) => {
 };
 
 export const getPlans = async (req, res) => {
-  console.log("here")
   try {
     const deeds = await Deed.find(
       {},
