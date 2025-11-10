c22bc854c1bd39003852ca3191c9c75536a440de
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-11 00:37:18 +0530

    update

--- src/index.js
+++ src/index.js
@@ -9,13 +9,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/deeds", deedRoutes);
