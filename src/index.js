0e0475b6dcb6ea75c650fd97a2e9fc831484793f
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-11-11 20:25:07 +0530

    test-mq

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import deedRoutes from "./routes/deedRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
+import { sendToQueue } from "./utils/producer.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/deeds", deedRoutes);
+
+const res = await sendToQueue({ event: "DeedServiceStarted", timestamp: new Date().toISOString() });
+console.log("Deed service start event sent to queue:", res);
 
 app.use(errorHandler);
 
